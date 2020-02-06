91b0c2c904f2505cc97c687813dd5740eace97bf
shicks1080 <stuart.hicks@gmail.com>
2020-02-06 16:28:02 +0100

    Centering the partner images in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from 'gatsby'
 
-import Agnesberg from '../images/sponsoragnesbergs.png'
+import AgnesbergSmall from '../images/sponsoragnesbergssmall.png'
 import Flyttdax from '../images/sponsorflyttdax.png'
 import Fysiken from '../images/sponsorfysiken.jpg'
 import McGlincheys from '../images/sponsormcglincheys.jpg'
@@ -12,9 +12,15 @@ export default () => (
   <div>
     <p className={footerStyles.footer}>We're grateful to our <Link to='/partners'>partners</Link> for their support:</p>
     <div className={footerStyles.footerLogos}>
-      <a href="http://www.agkungalv.se/" target="_blank" rel="noopener noreferrer"><img className={footerStyles.footerLogoBox} src={Agnesberg} alt="Agnesberg's logo" /></a>
-      <a href="http://www.flyttdax.se/" target="_blank" rel="noopener noreferrer"><img src={Flyttdax} alt="Flyttdax's logo" /></a>
-      <a href="https://fysiken.nu/sv/" target="_blank" rel="noopener noreferrer"><img src={Fysiken} alt="Fysiken's logo" /></a>
+      <a href="http://www.agkungalv.se/" target="_blank" rel="noopener noreferrer">
+        <img className={footerStyles.footerLogoBox} src={AgnesbergSmall} alt="Agnesberg's logo" />
+      </a>
+      <a href="http://www.flyttdax.se/" target="_blank" rel="noopener noreferrer">
+        <img src={Flyttdax} alt="Flyttdax's logo" />
+      </a>
+      <a href="https://fysiken.nu/sv/" target="_blank" rel="noopener noreferrer">
+        <img src={Fysiken} alt="Fysiken's logo" />
+      </a>
       <img src={McGlincheys} alt="McGlinchey's logo" />
     </div>
   </div>
