d7d0ec7139d3a84157589a1ce93efcecb10c1509
Stuart Hicks <stuart.hicks@gmail.com>
2020-02-06 12:51:38 +0100

    Updating the footer
    Adding the SRL sponsors' logos and inserting them into the footer. Making them flex.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,21 @@
 import React from "react"
+import { Link } from 'gatsby'
 
-import styles from "./footer.module.css"
-//import oLearysLogo from "../images/olearysnewlogo.jpg"
+import Agnesberg from '../images/sponsoragnesbergs.png'
+import Flyttdax from '../images/sponsorflyttdax.png'
+import Fysiken from '../images/sponsorfysiken.jpg'
+import McGlincheys from '../images/sponsormcglincheys.jpg'
+
+import footerStyles from "./footer.module.css"
 
 export default () => (
-  <div className={styles.footer}>
-    <p>Supported by <a href="https://olearys.se/lund/" target="_blank" rel="nofollow noopener noreferrer">O'Leary's</a> in Lund.</p>
-    
-    <p>Join us there after the game!</p>
+  <div>
+    <p className={footerStyles.footer}>We're grateful to our <Link to='/partners'>partners</Link> for their support:</p>
+    <div className={footerStyles.footerLogos}>
+      <a href="http://www.agkungalv.se/" target="_blank" rel="noopener noreferrer"><img className={footerStyles.footerLogoBox} src={Agnesberg} alt="Agnesberg's logo" /></a>
+      <a href="http://www.flyttdax.se/" target="_blank" rel="noopener noreferrer"><img src={Flyttdax} alt="Flyttdax's logo" /></a>
+      <a href="https://fysiken.nu/sv/" target="_blank" rel="noopener noreferrer"><img src={Fysiken} alt="Fysiken's logo" /></a>
+      <img src={McGlincheys} alt="McGlinchey's logo" />
+    </div>
   </div>
 )
