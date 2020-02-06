2ccc6fc75c01ec272efd42feac1c352557e42c7a
Stuart Hicks <stuart.hicks@gmail.com>
2020-02-06 12:52:59 +0100

    Adding the title area for the home screen.

---
+++ src/components/hometitle.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import styles from "./hometitle.module.css"
+
+export default (props) => (
+  <div className={styles.headerContainer}>
+    <div className={styles.brandname}>
+      <h1>{props.title}</h1>
+      <h3><i>{props.tagline}</i></h3>
+    </div>
+    <div className={styles.logo}>
+      <img className={styles.logo} src={props.imgSrc} alt={props.imgAlt} />
+    </div>
+  </div>
+)
