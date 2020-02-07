10f2c9e917effde02e59b5fe4a0e4269c7c8a1f6
shicks1080 <stuart.hicks@gmail.com>
2020-02-07 13:53:13 +0100

    Adding shop page plus pwa capability
    Adding the content for the merchandise page including shirt image.
    Adding gatsby-image plus associated plugins for presenting the shirt image satisfactorily.
    Adding the manifest and offline plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,76 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Sweden Rugby League,",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Sweden Rugby League",
+        short_name: "SRL",
+        start_url: "/",
+        background_color: "#004b87",
+        theme_color: "#333333",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png",
+        icons: [
+          {
+            src: "/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
--- src/pages/merchandise.js
+++ src/pages/merchandise.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import merchandiseStyles from './merchandise.module.css'
 
-export default () => (
-    <Layout pageTitle="Merchandise">
-        Link to shirt.
-        Hats?
-        Link to Stags shop.
-    </Layout>
+export default ({ data }) => (
+  <Layout pageTitle="Merchandise">
+    <Img
+			className={merchandiseStyles.shirtImage}
+      fluid={data.file.childImageSharp.fluid}
+      alt="The Sweden Rugby League team shirt."
+    />
+    <p className={merchandiseStyles.shopText}>
+      The national team's current kit is supplied by the <a
+        href="https://1908.store/pages/about-us"
+        target="_blank"
+        rel="noopener noreferrer"
+      >Baskerville project</a>
+      . If you are interested in getting your own to show you support for Sweden
+      rugby league then please visit them <a
+        href="https://1908.store/collections/frontpage/products/sweden-national-jersey"
+        target="_blank"
+        rel="noopener noreferrer"
+      >here</a>.
+    </p>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "srlshirt2018.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 450) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
