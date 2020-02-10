a819b05633e3eba727b036f813808ee162405886
shicks1080 <stuart.hicks@gmail.com>
2020-02-10 13:25:15 +0100

    Correct logo size on error page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,18 +3,23 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SrlLogo from "../images/srllogolarge.png"
+import errorStyles from './404.module.css'
 
 export default () => (
-  <Layout
-    pageTitle="Whoops!"
-  >
+  <Layout pageTitle="Whoops!">
     <h1>Whoops!</h1>
-    <img className="poster"
-      src={SrlLogo}
-      alt="Sweden Rugby League's Logo"
-    />
+    <div className={errorStyles.logo}>
+      <img
+        src={SrlLogo}
+        alt="Sweden Rugby League's Logo"
+      />
+    </div>
     <div className="bodyContent">
-      <p align="center">Sorry someone's dropped a ball somewhere as this doesn't appear to be the page you're looking for. You can return to the <Link to="/">home</Link> page or go back to the previous page.</p>
+      <p align="center">
+        Sorry someone's dropped a ball somewhere as this doesn't appear to be
+        the page you're looking for. You can return to the{" "}
+        <Link to="/">home</Link> page or go back to the previous page.
+      </p>
     </div>
   </Layout>
 )
