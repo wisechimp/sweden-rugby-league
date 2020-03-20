1ddcecf3e0e842bc52d70a3d6cbd5d5e0ad9bc90
shicks1080 <stuart.hicks@gmail.com>
2020-03-20 11:28:09 +0100

    Removing old sponsors
    Also increasing the size of the sponsor us image

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,7 @@ import React from "react"
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import { Link } from 'gatsby'
 
-import AgnesbergSmall from '../images/sponsoragnesbergssmall.png'
 import Flyttdax from '../images/sponsorflyttdax.png'
-import Fysiken from '../images/sponsorfysiken.jpg'
-import McGlincheys from '../images/sponsormcglincheys.jpg'
 import YourLogoHere from '../images/yourlogohere.png'
 
 import footerStyles from "./footer.module.css"
@@ -17,27 +14,12 @@ export default () => (
     </p>
     <div className={footerStyles.footerLogos}>
       <OutboundLink
-        href="http://www.agkungalv.se/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={AgnesbergSmall} alt="Agnesberg's logo" />
-      </OutboundLink>
-      <OutboundLink
         href="http://www.flyttdax.se/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img src={Flyttdax} alt="Flyttdax's logo" />
       </OutboundLink>
-      <OutboundLink
-        href="https://fysiken.nu/sv/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={Fysiken} alt="Fysiken's logo" />
-      </OutboundLink>
-      <img src={McGlincheys} alt="McGlinchey's logo" />
     </div>
     <Link to="/contact/">
       <img
