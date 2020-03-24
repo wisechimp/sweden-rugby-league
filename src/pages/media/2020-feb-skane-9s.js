1f787e7bcb63b9da884781262702230d6a6968b7
shicks1080 <stuart.hicks@gmail.com>
2020-03-24 10:29:59 +0100

    Adding image gallery page
    Files are stored on Cloudinary. Cloudinary transformations are included in this page.

--- src/pages/media/2020-feb-skane-9s.js
+++ src/pages/media/2020-feb-skane-9s.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import ImageGallery from 'react-image-gallery'
 
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 
 export default ({ data }) => {
   const { nodes } = data.allCloudinaryMedia
   console.log(nodes)
 
-  const images = nodes.map(({ secure_url }) => ({ original: secure_url, thumbnail: secure_url.replace("q_auto,f_auto", "h_150,w_250") }))
+  const images = nodes.map(({ secure_url }) => ({ 
+    original: secure_url, 
+    thumbnail: secure_url.replace("q_auto,f_auto", "h_150,w_250"), 
+    originalAlt: "Action shot from the Skane Rugby League 9's held in Malmo in February 2020" 
+  }))
 
   return (
     <Layout pageTitle="Skane 9's Feb 2020">
