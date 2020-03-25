7f414e98b6650c8384d3675fb30fa0085c83fb8d
shicks1080 <stuart.hicks@gmail.com>
2020-03-25 14:50:41 +0100

    Reducing the logo size
    Also removing a margin for the h3 tag which turned up from the media css module for some reason.

--- src/components/hometitle.js
+++ src/components/hometitle.js
@@ -6,7 +6,7 @@ export default (props) => (
   <div className={styles.headerContainer}>
     <div className={styles.brandname}>
       <h1>{props.title}</h1>
-      <h3><i>{props.tagline}</i></h3>
+      <h3 className={styles.tagline}><i>{props.tagline}</i></h3>
     </div>
     <div className={styles.logo}>
       <img className={styles.logo} src={props.imgSrc} alt={props.imgAlt} />
