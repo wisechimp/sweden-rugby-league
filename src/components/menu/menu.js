cb4968f280786a486f7d34bcc0c518bf521a9c37
shicks1080 <stuart.hicks@gmail.com>
2020-03-25 16:29:26 +0100

    Adding dropdown boxes to the menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -7,12 +7,30 @@ import menuStyles from './menu.module.css'
 
 const Menu =  props => {
   const [flippingBurger, setFlippingBurger] = useState(false)
+  const [newsDrop, setNewsDrop] = useState(false)
+  const [aboutDrop, setAboutDrop] = useState(false)
 
   const drawerHandler = () => {
     if (flippingBurger) {
       return setFlippingBurger(false)
     }
     setFlippingBurger(true)
+  }
+
+  const newsHandler = () => {
+    if (newsDrop) {
+      return setNewsDrop(false)
+    }
+    setNewsDrop(true)
+    setAboutDrop(false)
+  }
+
+  const aboutHandler = () => {
+    if (aboutDrop) {
+      return setAboutDrop(false)
+    }
+    setNewsDrop(false)
+    setAboutDrop(true)
   }
 
   return (
@@ -22,22 +40,43 @@ const Menu =  props => {
         flippingBurger={flippingBurger}
         setFlippingBurger={setFlippingBurger}
       />
-      <div className={`${menuStyles.navUnit} ${!flippingBurger ? '' : menuStyles.open}`} >
+      <div
+        className={`${menuStyles.navUnit} ${
+          !flippingBurger ? "" : menuStyles.open
+        }`}
+      >
         <ul className={menuStyles.navLinks}>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/latest-news">News</Link>
-          </li>
-          <li>
-            <Link to="/results">Results</Link>
+            <button onClick={newsHandler}>Latest +</button>
+            <div
+              className={`${menuStyles.dropdownContent} ${
+                !newsDrop ? "" : menuStyles.droppingDown
+              }`}
+            >
+              <Link to="/latest-news">News</Link>
+              <Link to="/media">Media</Link>
+              <Link to="results">Results</Link>
+            </div>
           </li>
           <li>
             <Link to="/clubs">Clubs</Link>
           </li>
           <li>
-            <Link to="/contact">About</Link>
+            <button onClick={aboutHandler}>About +</button>
+            <div
+              className={`${menuStyles.dropdownContent} ${
+                !aboutDrop ? "" : menuStyles.droppingDown
+              }`}
+            >
+              <Link to="/contact">Contact</Link>
+              <Link to="/what-is-rugby-league">Rugby League</Link>
+            </div>
+          </li>
+          <li>
+            <Link to="/merchandise">Shop</Link>
           </li>
         </ul>
         <div className={menuStyles.socialButts}>
