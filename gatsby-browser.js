dd63c84b7173ab80bc7b413222dc75321deb2d54
shicks1080 <stuart.hicks@gmail.com>
2020-03-27 14:20:33 +0100

    Adding the colour scheme as a set of CSS variables
    Refactoring all the stylesheets that use the default colour scheme.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "react-image-gallery/styles/css/image-gallery.css"
+import "./src/styles/variables.css"
 import "./src/styles/sweden-rugby-league-global-styles.css"
