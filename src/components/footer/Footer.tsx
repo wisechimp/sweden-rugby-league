8cb4d417a10ddfac41e3cebec884ad1c739ae568
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2024-12-09 09:26:45 +0100

    feat: Adding a footer that fetches data from Sanity

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,40 @@
+import { getPartners } from "@/sanity/sanity-utils"
+import Image from "next/image"
+import Link from "next/link"
+
+import * as styles from './footer.module.css'
+
+const Footer = async () => {
+  const partnersData = await getPartners()
+
+  return (
+    <div className={styles.footerContainer}>
+      <h2>
+        We are grateful to our partners for their support:
+      </h2>
+      <div className={styles.footerPartnersContainer}>
+        {partnersData.map(partner => (
+          <div key={partner._id}>
+            <Image
+            src={partner.logo}
+            alt={partner.logoAltText}
+            width={200}
+            height={100}
+            />
+          </div>
+        ))}
+      </div>
+      <div className={styles.footerYourName}>
+        <p>
+          <strong>Your Logo Here!</strong>
+        </p>
+        <br />
+        <p className={styles.footerLink}>
+            <Link href="/contact">Click</Link> for more information. 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
