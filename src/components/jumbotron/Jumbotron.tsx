c4659decc8d5d0ed17bf926b60025f2ebd1a96ff
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2026-03-03 17:08:41 +0100

    feat: Removing the caption from the hero image
    May serve as a distraction

--- src/components/jumbotron/Jumbotron.tsx
+++ src/components/jumbotron/Jumbotron.tsx
@@ -1,21 +1,18 @@
-import Image, { StaticImageData } from "next/image"
+import Image, { StaticImageData } from 'next/image'
 
 import * as styles from './jumbotron.module.css'
 
 type JumbotronProps = {
-  imageSrc: StaticImageData,
+  imageSrc: StaticImageData
   imageAltTxt: string
 }
 
 const Jumbtron = ({ imageSrc, imageAltTxt }: JumbotronProps) => {
   return (
     <div>
-      <Image
-        src={imageSrc}
-        alt={imageAltTxt}
-        priority
-      />
-      <p className={styles.captionBox}>A pacy hit up against Norway in 2017</p>
+      <div className={styles.jumbotronImage}>
+        <Image src={imageSrc} alt={imageAltTxt} priority />
+      </div>
     </div>
   )
 }
