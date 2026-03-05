d1683b5e079e4fb836414c7a2237caa32841eca0
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2026-03-05 15:14:26 +0100

    feat: Adding to the menu
    Logo
    Important links on right hand side
    Styling so the logo appears "above" the popover (it doesn't!)
    Spreading the parts of the menu out so they don't squidge each other,

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,12 +1,26 @@
+import Image from 'next/image'
 import * as styles from './menu.module.css'
 import MenuDropdownButton from './menu-dropdown-button'
+import Logo from '../../images/srllogo.png'
+import Link from 'next/link'
 
 const Menu = () => {
   return (
     <nav className={styles.menuContainer}>
-      <MenuDropdownButton menu="National Team" />
-      <MenuDropdownButton menu="Clubs" />
-      <MenuDropdownButton menu="About" />
+      <div className={styles.menuContainerLeft}>
+        <MenuDropdownButton menu="National Team" />
+        <MenuDropdownButton menu="Clubs" />
+        <MenuDropdownButton menu="About" />
+      </div>
+      <div className={styles.menuContainerCenter}>
+        <Link href="/">
+          <Image src={Logo} alt="Sweden Rugby League's Logo featuring the three crowns." />
+        </Link>
+      </div>
+      <div className={styles.menuContainerRight}>
+        <Link href="./contact">Contact</Link>
+        <Link href="./get-involved">Join Us!</Link>
+      </div>
     </nav>
   )
 }
--- src/components/menu/menu-dropdown-button.tsx
+++ src/components/menu/menu-dropdown-button.tsx
@@ -1,7 +1,11 @@
 import { fetchMenu } from '@/actions'
 import MenuItem from './MenuItem'
 
+import Logo from '../../images/srllogo.png'
+
 import * as styles from './menu.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 type MenuDropdownButtonType = {
   menu: string
@@ -18,6 +22,11 @@ const MenuDropdownButton = ({ menu }: MenuDropdownButtonType) => {
       </button>
       <div id={menupopover} popover="" className={styles.menuDropdownContainer}>
         <MenuItem data={menuData!} menuPopover={menupopover} />
+        <div className={styles.menuDropdownLogo}>
+          <Link href="/">
+            <Image src={Logo} alt="Sweden Rugby League's Logo featuring the three crowns." />
+          </Link>
+        </div>
       </div>
     </div>
   )
