e6d5ea37b7b6b66ff10c03457cc11c5969c4fab8
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2026-03-12 12:39:04 +0100

    styling: Adding a SVG arrow for the dropdown menu
    And rotating it on opening the dropdown.

--- src/components/menu/menu-dropdown-button.tsx
+++ src/components/menu/menu-dropdown-button.tsx
@@ -1,35 +1,71 @@
-import { fetchMenu } from '@/actions'
-import MenuItem from './MenuItem'
+"use client";
 
-import Logo from '../../images/srllogo.png'
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
-import * as styles from './menu.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
+import { fetchMenu } from "@/actions";
+
+import Logo from "../../images/srllogo.png";
+import * as styles from "./menu.module.css";
+import MenuItem from "./MenuItem";
 
 type MenuDropdownButtonType = {
-  menu: string
-}
+  menu: string;
+};
 
 const MenuDropdownButton = ({ menu }: MenuDropdownButtonType) => {
-  const menupopover = `menupopover-${menu}`
-  const menuData = fetchMenu(menu)
+  const [iconRotated, setIconRotated] = useState(false);
+  const menupopover = `menupopover-${menu}`;
+  const menuData = fetchMenu(menu);
+  const rotateIcon = () => {
+    setIconRotated(!iconRotated);
+  };
 
   return (
     <div className={styles.menuButtonContainer}>
-      <button popoverTarget={menupopover} popoverTargetAction="toggle">
+      <button
+        popoverTarget={menupopover}
+        popoverTargetAction="toggle"
+        onClick={rotateIcon}
+        className={styles.menuButton}
+      >
         {menu}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          viewBox="0 0 20 20"
+          fill="none"
+          nav-link-icon=""
+          className={styles.menuNavIcon}
+        >
+          <path
+            transform={
+              !iconRotated
+                ? "translate(-2.6, -1.3)"
+                : "translate(2.6, 1.3) rotate(180 10 10)"
+            }
+            d="M6.5 9L12.5 15L18.5 9"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
       </button>
       <div id={menupopover} popover="" className={styles.menuDropdownContainer}>
         <MenuItem data={menuData!} menuPopover={menupopover} />
         <div className={styles.menuDropdownLogo}>
           <Link href="/">
-            <Image src={Logo} alt="Sweden Rugby League's Logo featuring the three crowns." />
+            <Image
+              src={Logo}
+              alt="Sweden Rugby League's Logo featuring the three crowns."
+            />
           </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MenuDropdownButton
+export default MenuDropdownButton;
