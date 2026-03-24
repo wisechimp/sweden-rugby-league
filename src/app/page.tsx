677d767fb28cd36c07c1bf861847a7d410205380
Stuart Hicks <stuart.hicks@gmail.com>
2026-03-24 12:02:48 +0100

    feat: Adding clubs to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import Jumbtron from '@/components/jumbotron/Jumbotron'
-import norwayCharge from '../images/norwaycharge.jpg'
+import Clubcard from "@/components/club-card/clubcard";
+import Jumbtron from "@/components/jumbotron/Jumbotron";
+import clubData from "@/data/clubs/clubs";
+
+import norwayCharge from "../images/norwaycharge.jpg";
 
 const HomePage = () => {
   return (
@@ -10,23 +13,39 @@ const HomePage = () => {
         imageSrc={norwayCharge}
         imageAltTxt="A Sweden player charges towards the Norwegian defenders with the ball"
       />
-      <div className={'pageMainParagraph'}>
+      <div className={"pageMainParagraph"}>
         <p>
-          Sweden Rugby League was founded in 2009 with the aim to introduce and grow the sport of{' '}
-          <Link href="/rugby-league">rugby league</Link> in Sweden. They were admitted to the{' '}
-          <Link href="https://europeanrugbyleague.com/" target="_blank" rel="noopener noreferrer">
+          Sweden Rugby League was founded in 2009 with the aim to introduce and
+          grow the sport of <Link href="/rugby-league">rugby league</Link> in
+          Sweden. They were admitted to the{" "}
+          <Link
+            href="https://europeanrugbyleague.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             European Rugby League
-          </Link>{' '}
-          in 2011 as Observer members. They oversee the{' '}
-          <Link href="/clubs">national competition</Link> in Sweden and the national team who were{' '}
-          <Link href="https://intrl.sport/world-rankings" target="_blank" rel="noopener noreferrer">
+          </Link>{" "}
+          in 2011 as Observer members. They oversee the{" "}
+          <Link href="/clubs">national competition</Link> in Sweden and the
+          national team who were{" "}
+          <Link
+            href="https://intrl.sport/world-rankings"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             ranked 32nd
-          </Link>{' '}
+          </Link>{" "}
           in the world as recently as 2022.
         </p>
       </div>
+      <h2 className="subHeader">Our Clubs</h2>
+      <div className="flexCards">
+        {clubData.map((club) => {
+          return <Clubcard key={club.key} data={club} />;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
